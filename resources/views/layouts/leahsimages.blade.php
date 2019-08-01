cddc085f9f9f4429f7ee7fb2d66a63e5ae6814ed
Laura Tyler <laura@jofa.co.uk>
2019-08-01 14:53:13 +0100

    this puts the javascript local

--- resources/views/layouts/leahsimages.blade.php
+++ resources/views/layouts/leahsimages.blade.php
@@ -4,9 +4,9 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Leah's Images:{{$title}}</title>
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
+  <link rel="stylesheet" href="/css/jquery-ui.css" />
   <link rel="stylesheet" href="/css/bootstrap.min.css" media="screen" />
-  <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.css" rel="stylesheet" />
+  <link href="/css/summernote.css" rel="stylesheet" />
   <link rel="stylesheet" href="/css/leahsimages.css" />
   <link rel="icon" href="/favicon.ico" type="image/x-icon" />
   
@@ -16,10 +16,10 @@
 
 @yield('content')
 
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="http://cdn.sobekrepository.org/includes/jquery-rotate/2.2/jquery-rotate.min.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.js"></script>
+<script src="/js/jquery-1.12.4.js"></script>
+<script src="/js/jquery-rotate.min.js"></script>
+  <script src="/js/jquery-ui.js"></script>
+<script src="/js/summernote.js"></script>
 <script src="/js/bootstrapmin.js"></script>
 <script src="/js/leahsimages.js"></script>
 </body>
